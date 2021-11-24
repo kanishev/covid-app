1b47cbf121df9dff8561387d0733ce92bed3c003
Nick <50417170+kanishev@users.noreply.github.com>
2021-11-24 23:21:52 +0300

    create base components / add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main class="main">
+      <TheHeader />
+      <AppRate />
+      <AppChart />
+      <AppTable />
+      <TheFooter />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from "./components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
+import AppChart from "./components/AppChart.vue";
+import AppTable from "./components/AppTable.vue";
+import AppRate from "./components/AppRate.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { TheHeader, TheFooter, AppChart, AppTable, AppRate },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
