852b9923dfc391aa663363e28332c8c4b5179b8c
Nick <50417170+kanishev@users.noreply.github.com>
2021-11-25 23:44:14 +0500

    add rate cards

--- src/components/AppRate.vue
+++ src/components/AppRate.vue
@@ -1,5 +1,42 @@
 <template>
-  <h2 class="text-sm">Rate</h2>
+  <div class="container mx-auto mt-12">
+    <div class="rate-list flex space-x-5 justify-between">
+      <div class="card-1 flex-1 shadow bg-white rounded text-center p-4">
+        <span class="label uppercase text-gray-500 font-semibold text-base"
+          >Все случаи</span
+        >
+        <div class="value text-indigo-400 font-black text-4xl py-2">100000</div>
+        <div class="value-2 text-indigo-400 font-semibold text-xl">
+          <span>+</span>
+          23
+        </div>
+      </div>
+
+      <div class="card-1 flex-1 shadow bg-white rounded text-center p-4">
+        <span class="label uppercase text-gray-500 font-semibold text-base"
+          >Все случаи</span
+        >
+        <div class="value text-red-500 font-black text-5xl py-2">102030000</div>
+        <div class="value-2 text-red-500 font-semibold text-xl">
+          <span>+</span>
+          23
+        </div>
+      </div>
+
+      <div class="card-1 flex-1 shadow bg-white rounded text-center p-4">
+        <span class="label uppercase text-gray-500 font-semibold text-base"
+          >Все случаи</span
+        >
+        <div class="value text-green-500 font-black text-5xl py-2">
+          10121312
+        </div>
+        <div class="value-2 text-green-500 font-semibold text-xl">
+          <span>+</span>
+          400
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
